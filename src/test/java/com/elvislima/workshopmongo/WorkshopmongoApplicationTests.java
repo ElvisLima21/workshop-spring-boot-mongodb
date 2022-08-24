bb292156138cc1efdb7454031e5981a85434aefb
Elvis Lima <elvislimagit2145@gmail.com>
2022-08-24 16:05:42 -0300

    First commit - project created

---
+++ src/test/java/com/elvislima/workshopmongo/WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elvislima.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
